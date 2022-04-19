8dbf3ab0a745e30dce18c2065f96d423337c1619
DysonBL <henrydysonj@dorustree.in>
2022-04-19 17:38:10 +0530

    lazylozd

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import { Suspense } from "react";
 // import Dyson from "../src/Components/Dyson";
 // import Felix from "../src/Components/Felix";
 // import Karthi from "../src/Components/Karthi";
-import Mani from "../src/Components/Mani";
+// import Mani from "../src/Components/Mani";
 import Stanly from "../src/Components/Stanly";
 import Friends from "../src/Friends";
 
 const Dyson = React.lazy(() => import("./Components/Dyson"));
 const Felix = React.lazy(() => import("./Components/Felix"));
 const Karthi = React.lazy(() => import("./Components/Karthi"));
+const Mani = React.lazy(() => import("./Components/Mani"));
+
 
 
 const App = () => {
